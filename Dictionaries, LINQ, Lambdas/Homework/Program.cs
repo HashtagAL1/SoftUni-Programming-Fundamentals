e4fbbd17a7dd8bd506387cf6ba887963fc435b75
Anastas Laskov <anastas.luskov@abv.bg>
2017-10-16 01:07:02 +0300

    Second part of the homework

--- Dictionaries, LINQ, Lambdas/Homework/Program.cs
+++ Dictionaries, LINQ, Lambdas/Homework/Program.cs
@@ -10,8 +10,207 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            MinerTask();
 
+        }
+
+        public static void LegendaryFarming()
+        {
+            var legendaryItems = new Dictionary<string, uint>
+            {
+                {"shards", 0 },
+                { "fragments", 0},
+                { "motes", 0}
+            };
+            var result = new Dictionary<string, string>
+            {
+                {"shards", "Shadowmourne" },
+                {"fragments", "Valanyr" },
+                {"motes", "Dragonwrath" }
+            };
+            var junkItems = new Dictionary<string, uint>();
+            int cnt = 0;
+            bool IsFound = false;
+            string legendaryItem = "";
+            string flag = "";
+
+            while (cnt <= 10)
+            {
+                cnt++;
+                string[] input = Console.ReadLine().ToLower().Split(' ');
+                for (int i = 1, j = i - 1; i < input.Length; i += 2, j += 2)
+                {
+                    if (input[i].Equals("shards") || input[i].Equals("motes") || input[i].Equals("fragments"))
+                    {
+                        legendaryItems[input[i]] += uint.Parse(input[j]);
+                        if (legendaryItems[input[i]] >= 250)
+                        {
+                            legendaryItem = result[input[i]];
+                            flag = input[i];
+                            IsFound = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (!(junkItems.ContainsKey(input[i])))
+                        {
+                            junkItems[input[i]] = 0;
+                        }
+                        junkItems[input[i]] += uint.Parse(input[j]);
+                    }
+
+                }
+                if (IsFound)
+                {
+                    break;
+                }
+            }
+            legendaryItems[flag] -= 250;
+            var sortedLegItems = legendaryItems.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+            var sortedJunk = junkItems.OrderBy(x => x.Key);
+
+            Console.WriteLine($"{legendaryItem} obtained!");
+
+            foreach (var legItem in sortedLegItems)
+            {
+
+                Console.WriteLine(legItem.Key + ": " + legItem.Value);
+            }
+            foreach (var junk in sortedJunk)
+            {
+                Console.WriteLine(junk.Key + ": " + junk.Value);
+            }
+        }
+
+        public static void PrintStringList(List<string> list)
+        {
+            list.Sort();
+            list = list.Distinct().ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == list.Count - 1)
+                {
+                    Console.Write($"{list[i]}");
+                }
+                else
+                {
+                    Console.Write($"{list[i]}, ");
+                }
+            }
+        }
+
+        public static long GetSum(List<long> list)
+        {
+            long sum = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i];
+            }
+            return sum;
+        }
+
+        public static void PopulationCounter()
+        {
+            var countries = new Dictionary<string, Dictionary<string, long>>();//country, <city, city population>
+            string[] input = Console.ReadLine().Split('|');
+            if (input.Length <= 1 || input[0].Equals("report"))
+            {
+                return;
+            }
+
+            while (!(input[0].Equals("report")))
+            {
+                string cityName = input[0];
+                string countryName = input[1];
+                long population = long.Parse(input[2]);
+
+                if (!(countries.ContainsKey(countryName)))
+                {
+                    countries[countryName] = new Dictionary<string, long>();
+                }
+                if (!(countries[countryName].ContainsKey(cityName)))
+                {
+                    countries[countryName][cityName] = population;
+                }
+
+                input = Console.ReadLine().Split('|');
+                if (input.Length <= 1)
+                {
+                    break;
+                }
+            }
+
+            var sorted = countries.OrderByDescending(x => GetSum(x.Value.Values.ToList()));
+            foreach (var country in sorted)
+            {
+                var ordered = country.Value.OrderByDescending(x => x.Value);
+                Console.WriteLine($"{country.Key} (total population: {GetSum(country.Value.Values.ToList())})");
+                foreach (var city in ordered)
+                {
+                    Console.WriteLine($"=>{city.Key}: {city.Value}");
+                }
+            }
+        }
+
+        public static void UserLogs()
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ', '=', '\'' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var users = new SortedDictionary<string, Dictionary<string, int>>();
+
+            if (input.Length <= 1)
+            {
+                Console.WriteLine();
+                return;
+            }
+            while (!(input[0].Equals("end")))
+            {
+                string userIP = input[1];
+                string message = input[3];
+                string userName = input[5];
+
+                if (!(users.ContainsKey(userName)))
+                {
+                    users[userName] = new Dictionary<string, int>();
+                }
+
+                if (!(users[userName].ContainsKey(userIP)))
+                {
+                    users[userName][userIP] = 0;
+                }
+
+                users[userName][userIP]++;
+
+
+                input = Console.ReadLine().Split(new char[] { ' ', '=' }, StringSplitOptions.RemoveEmptyEntries);
+                if (input.Length <= 1 || input[0].Equals("end"))
+                {
+                    break;
+                }
+            }
+            foreach (var user in users)
+            {
+
+                List<string> res = new List<string>();
+                Console.WriteLine(user.Key + ": ");
+
+                foreach (var ip in users[user.Key])
+                {
+                    res.Add(ip.Key + " => " + ip.Value);
+                }
+
+                for (int j = 0; j < res.Count; j++)
+                {
+                    if (j == res.Count - 1)
+                    {
+                        Console.WriteLine(res[j] + ".");
+                    }
+                    else
+                    {
+                        Console.Write(res[j] + ", ");
+                    }
+                }
+            }
 
         }
 
@@ -150,7 +349,6 @@ namespace Homework
             var emails = new Dictionary<string, string>();
             while (!(name.Equals("stop")))
             {
-                //if domain == us || uk don't add them - add the rest
                 string domain = email[email.Length - 3] + "" + email[email.Length - 2] + "" + email[email.Length - 1];
                 if (!(domain.Equals(".us") || domain.Equals(".uk")))
                 {
@@ -213,6 +411,39 @@ namespace Homework
             }
 
             Console.WriteLine(string.Join("\n", players.Select(item => item.Key + ": " + item.Value)));
+        }
+
+        public static void LogsAggregator()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var userAndTime = new SortedDictionary<string, uint>();
+            var userAndIPs = new Dictionary<string, List<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string userName = input[1];
+                string ip = input[0];
+                uint duration = uint.Parse(input[2]);
+
+                if (!(userAndTime.ContainsKey(userName)))
+                {
+                    userAndTime[userName] = 0;
+                }
+                if (!(userAndIPs.ContainsKey(userName)))
+                {
+                    userAndIPs[userName] = new List<string>();
+                }
+
+                userAndTime[userName] += duration;
+                userAndIPs[userName].Add(ip);
+            }
+
+            foreach (var user in userAndTime)
+            {
+                Console.Write($"{user.Key}: {user.Value} [");
+                PrintStringList(userAndIPs[user.Key]);
+                Console.WriteLine("]");
+            }
         }
     }
 }
