2c64a7febd479d44b34c477d568782ff4290e6f8
Anastas Laskov <anastas.luskov@abv.bg>
2017-10-23 20:51:43 +0300

    Exam preparation

---
+++ PastExam9july/PokeMon/PM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long power = long.Parse(Console.ReadLine());
+            long distance = long.Parse(Console.ReadLine());
+            long exhFactor = long.Parse(Console.ReadLine());
+            long cnt = 0;
+            long temp = power / 2;
+
+            while (power >= distance)
+            {
+                power -= distance;
+                cnt++;
+                if (power == temp)
+                {
+                    if (exhFactor != 0)
+                    {
+                        power /= exhFactor;
+                    }
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(cnt);
+        }
+    }
+}
---
+++ PastExam9july/PokemonDontGo/PMDG.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonDontGo
+{
+    class PMDG
+    {
+        static void Main(string[] args)
+        {
+            List<long> input = Console.ReadLine().Split(' ').Select(long.Parse).ToList();
+            List<long> removedElements = new List<long>();
+
+            while (input.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+                if (index < 0)
+                {
+                    long num = input[0];
+                    removedElements.Add(num);
+                    input.RemoveAt(0);
+
+                    OperateOnList(input, num);
+
+                    input.Insert(0, input[input.Count - 1]);
+                }
+                else if (index >= input.Count)
+                {
+                    long num = input[input.Count - 1];
+                    removedElements.Add(num);
+                    input.RemoveAt(input.Count - 1);
+
+                    OperateOnList(input, num);
+
+                    long addon = input[0];
+                    input.Add(addon);
+                }
+                else
+                {
+                    long num = input[index];
+                    removedElements.Add(num);
+                    input.RemoveAt(index);
+
+                    OperateOnList(input, num);
+                }
+            }
+
+            long sum = removedElements.Sum();
+            Console.WriteLine(sum);
+        }
+
+        public static void OperateOnList(List<long> input, long num)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] > num)
+                {
+                    input[i] -= num;
+                }
+                else
+                    input[i] += num;
+            }
+        }
+    }
+}
